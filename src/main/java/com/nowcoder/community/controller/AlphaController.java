2558f11e246dcf4bc3204483f00684c40724508f
李晓秋 <TranpriMit@gmail.com>
2021-03-27 15:35:59 +0800

    MVC build simple web pages

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -3,16 +3,22 @@ package com.nowcoder.community.controller;
 import com.nowcoder.community.service.AlphaService;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Enumeration;
-import javax.servlet.ServletOutputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author Hastur
@@ -67,4 +73,67 @@ public class AlphaController {
         System.out.println(limit);
         return "some students";
     }
+
+    // /student/123
+    @RequestMapping(path = "/student/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String getStudent(@PathVariable("id") int id){
+        System.out.println(id);
+        return "a student";
+    }
+
+    //POST请求
+    @RequestMapping(path = "/student", method = RequestMethod.POST)
+    @ResponseBody
+    public String saveStudent(String name, int age){
+        System.out.println(name);
+        System.out.println(age);
+        return  "student";
+    }
+
+    // 响应html数据
+
+    @RequestMapping(path = "/teacher", method = RequestMethod.GET)
+    public ModelAndView getTeacher(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("name","张三");
+        modelAndView.addObject("age",30);
+        modelAndView.setViewName("/demo/view");
+        return modelAndView;
+    }
+
+    @RequestMapping(path = "/school", method = RequestMethod.GET)
+    public String getSchool(Model model){
+        model.addAttribute("name", "北京大学");
+        model.addAttribute("age", 80);
+        return "/demo/view";
+    }
+
+    //响应json数据（异步请求）
+    //java对象 -> Json字符串 -> JS对象
+
+    @RequestMapping(path = "/emp", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Map<String, Object>> getEmp(){
+        List<Map<String,Object>> emps = new ArrayList<>();
+        Map<String, Object> emp = new HashMap<>();
+        emp.put("name","张三");
+        emp.put("age",23);
+        emp.put("salary",8000);
+        emps.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name","李四");
+        emp.put("age",24);
+        emp.put("salary",9000);
+        emps.add(emp);
+
+        emp = new HashMap<>();
+        emp.put("name","王五");
+        emp.put("age",23);
+        emp.put("salary",10000);
+        emps.add(emp);
+        return emps;
+    }
+
 }
